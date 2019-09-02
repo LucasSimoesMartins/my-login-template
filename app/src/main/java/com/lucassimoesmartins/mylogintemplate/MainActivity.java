112194a8b648ce5ce3ccbae3963d4c546e8880b1
LucasSimoesMartins <lucassimoes084@gmail.com>
2019-09-02 16:58:04 -0300

    Added other layout elements

--- app/src/main/java/com/lucassimoesmartins/mylogintemplate/MainActivity.java
+++ app/src/main/java/com/lucassimoesmartins/mylogintemplate/MainActivity.java
@@ -2,14 +2,58 @@ package com.lucassimoesmartins.mylogintemplate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText edtEmail, edtPassword;
+    Button btnLogin, btnFacebook, btnGoogle;
+    TextView txtForgotPassword, txtSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtEmail = findViewById(R.id.edtEmail);
+        edtPassword = findViewById(R.id.edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnFacebook = findViewById(R.id.btn_facebook);
+        btnGoogle = findViewById(R.id.btn_google);
+        txtForgotPassword = findViewById(R.id.txtForgotPassword);
+        txtSignUp = findViewById(R.id.txtSignUp);
+
+        btnLogin.setOnClickListener(this);
+        btnFacebook.setOnClickListener(this);
+        btnGoogle.setOnClickListener(this);
+        txtForgotPassword.setOnClickListener(this);
+        txtSignUp.setOnClickListener(this);
+
         getSupportActionBar().hide();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.btnLogin:
+                //Call Login method here
+                break;
+            case R.id.btn_facebook:
+                //Call Facebook login method here
+                break ;
+            case R.id.btn_google:
+                //Call Google login method here
+                break ;
+            case R.id.txtForgotPassword:
+                //Call Forgot password method here
+                break ;
+            case R.id.txtSignUp:
+                //Call Sign Up method here
+                break;
+        }
     }
 }
